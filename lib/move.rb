8af213deff9406a484c80b7c6fb7bb80922388bf
Jasper De Luna <jasper.deluna@aol.com>
2018-10-04 12:05:45 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,11 +11,11 @@ end
 def input_to_index(user_input)
   new_user_input = user_input.to_i
   new_user_input -= 1
-  return 
+  return new_user_input
 end
 
 def move(board, index, character = "X")
   board[index] = character
-  return 
-end
+  return board
+
 
